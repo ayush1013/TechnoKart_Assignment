6b3f3291f0436a69e939d7385f28a3bd36401084
Ayush Verma <ayush98277@gmail.com>
2023-10-28 18:49:29 +0530

    all the invoces dispayed on UI

--- invoice-app/src/Pages/Home.jsx
+++ invoice-app/src/Pages/Home.jsx
@@ -1,9 +1,112 @@
-import React from 'react'
+import {
+  Box,
+  CircularProgress,
+  Flex,
+  Grid,
+  Heading,
+  Text,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import { getData } from "../Redux/action";
+import { useDispatch, useSelector } from "react-redux";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const invoices = useSelector((store) => store.products);
+  const isLoading = useSelector((store) => store.isLoading);
+  const dispatch = useDispatch();
 
-export default Home
+  console.log("isLoading", isLoading);
+  console.log("invoices", invoices);
+
+  useEffect(() => {
+    dispatch(getData());
+  }, []);
+
+  if (isLoading) return <CircularProgress isIndeterminate size="120px" />;
+
+  return (
+    <Box>
+      <Grid w={"100%"} gridTemplateColumns="repeat(2,1fr)" gap="50px" p="20px">
+        {invoices?.length > 0 &&
+          invoices?.map((elem) => (
+            <Box key={elem._id} boxShadow={"lg"} p="20px" border="1px solid grey" >
+              <Heading fontSize={"24px"}>INVOICE</Heading>
+              <Flex
+                mt="20px"
+                justifyContent={"space-between"}
+                w="100%"
+                fontSize={"14px"}
+              >
+                <Box textAlign={"left"}>
+                  <Text fontWeight={"bold"}>BILLED TO:</Text>
+                  <Text>Pooja EnterPrises</Text>
+                  <Text>+91 8103153152</Text>
+                  <Text>187/A Geeta Bhawan, Indore, MP</Text>
+                </Box>
+                <Box textAlign={"left"}>
+                  <Text fontWeight={"bold"}>Invoice No. {elem.number}</Text>
+                  <Text>{elem.invoiceDate.split("T").map(String)[0]}</Text>
+                </Box>
+              </Flex>
+
+
+              <TableContainer mt="20px" >
+                <Table variant="simple">
+                  <Thead>
+                    <Tr>
+                      <Th>Item</Th>
+                      <Th>Quantity</Th>
+                      <Th isNumeric>Unit Price</Th>
+                      <Th>Total</Th>
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    <Tr>
+                      <Td>Cuban Collar Shirt</Td>
+                      <Td>{Math.floor(elem.invoiceAmount/100)}</Td>
+                      <Td >$100</Td>
+                      <Td>${elem.invoiceAmount}</Td>
+                    </Tr>
+                  </Tbody>
+                </Table>
+              </TableContainer>
+
+              <Text textAlign={"left"} mt="20px"  fontWeight={"bold"}  >
+                Thank You!
+              </Text>
+            
+              <Flex
+                mt="20px"
+                justifyContent={"space-between"}
+                w="100%"
+                fontSize={"14px"}
+              >
+                <Box textAlign={"left"}>
+                    <Text fontWeight={"bold"}>Payment Information</Text>
+                  <Text>SBI Bank</Text>
+                  <Text>Account Name: Ayush Verma</Text>
+                  <Text>Account No. 71000313152456</Text>
+                  <Text>Pay By {elem.invoiceDate.split("T").map(String)[0]}</Text>
+                </Box>
+                <Box textAlign={"left"}>
+                  <Text fontWeight={"bold"}> Ayush Verma</Text>
+                  <Text>123, New Palasia, Indore, MP</Text>
+                </Box>
+              </Flex>
+            </Box>
+          ))}
+      </Grid>
+    </Box>
+  );
+};
+
+export default Home;
--- invoice-app/src/Redux/action.js
+++ invoice-app/src/Redux/action.js
@@ -1,15 +1,17 @@
 import * as types from "./actionTypes";
 import axios from "axios";
 
-export const getData = (params)=> (dispatch)=>{
-    dispatch({type: types.GET_DATA_REQUEST});
-    axios.get(`https://tough-fish-hem.cyclic.app/api${params}`)
-        .then((res)=>{
-            console.log("data from action",res.data);
-            dispatch({type: types.GET_DATA_SUCCESS, payload:res.data});
-        })
-        .catch((err)=>{
-            console.log(err);
-            dispatch({type: types.GET_DATA_ERROR});
-        })
-}
+export const getData = () => (dispatch) => {
+    // console.log(params)
+  dispatch({ type: types.GET_DATA_REQUEST });
+  axios
+    .get(`https://tough-fish-hem.cyclic.app/api`)
+    .then((res) => {
+      console.log("data from action", res.data.data);
+      dispatch({ type: types.GET_DATA_SUCCESS, payload: res.data.data });
+    })
+    .catch((err) => {
+      console.log(err);
+      dispatch({ type: types.GET_DATA_ERROR });
+    });
+};
